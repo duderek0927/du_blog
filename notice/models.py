4c10cf74e2cccb234a1c8fd2d94a1dfd33159ba9
derek <dsalli0927@163.com>
2020-04-09 10:23:36 +0800

    xxx

---
+++ notice/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+from django.urls import path
+# 引入views.py
+from . import views
+
+app_name = 'notice'
+
+urlpatterns = [
+    # 通知列表
+    path('list/', views.CommentNoticeListView.as_view(), name='list'),
+    path('update/', views.CommentNoticeUpdateView.as_view(), name='update'),
+]
